68fad2e1ad2520b06e5918c77dd2b76f6a0ab005
Joe Carlson <Josephcarlson1@gmail.com>
2013-12-02 23:23:17 -0600

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CallMeJoe::Application.config.secret_key_base = '50f1eb7c8ccbd313cc843233df2270e9a562bc0aaf791883741b4021a03149aac161af3b5d5270b4ae9baa2a785fca3e95e190f575bb5dc3a6f125d958f0bd02'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CallMeJoe::Application.config.session_store :cookie_store, key: '_call_me_joe_session'
