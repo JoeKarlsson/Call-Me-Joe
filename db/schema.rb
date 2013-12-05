817ce91fa12663ec7afa77cb1263d0d647faa484
Joe Carlson <Josephcarlson1@gmail.com>
2013-12-05 13:55:08 -0600

    convert sqlite to postgres

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20131205011243) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "microposts", force: true do |t|
     t.string   "content"
     t.integer  "user_id"
