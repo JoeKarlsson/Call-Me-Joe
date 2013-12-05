514533cda88690863d59f237335deb7ee4300c35
Joe Carlson <Josephcarlson1@gmail.com>
2013-12-05 01:05:00 -0600

    convert sqlite db to postgresql

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  # Returns the full title on a per-page basis.       # Documentation comment
+  def full_title(page_title)                          # Method definition
+    base_title = "Call Me Joe"  # Variable assignment
+    if page_title.empty?                              # Boolean test
+      base_title                                      # Implicit return
+    else
+      "#{base_title} | #{page_title}"                 # String interpolation
+    end
+  end
 end
