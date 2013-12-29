f9e424b6dedaf458ebc8566313e679be259040be
Joe Carlson <Josephcarlson1@gmail.com>
2013-12-29 16:40:48 -0600

    Version 1.0 complete. Ready for launch

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -9,7 +9,7 @@ class MicropostsController < ApplicationController
       redirect_to root_url
     else
       @feed_items = []
-      render 'static_pages/home'
+      render 'static_pages/microposts'
     end
   end
 
