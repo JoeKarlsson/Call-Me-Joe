32b443a76fc9161ac16c5d044b2868f63447e7d3
Joe Carlson <Josephcarlson1@gmail.com>
2014-01-23 16:48:31 -0600

    fixed html editor 8

--- config/application.rb
+++ config/application.rb
@@ -26,7 +26,5 @@ module CallMeJoe
 
     #config ckeditor
     config.autoload_paths += %W(#{config.root}/app/models/ckeditor)
-    config.assets.precompile += Ckeditor.assets
-    config.assets.precompile += %w(ckeditor/*)
   end
 end
