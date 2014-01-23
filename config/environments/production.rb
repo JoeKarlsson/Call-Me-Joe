4ce4eb99fd7415e8b81190f634b85d9d5de80f69
Joe Carlson <Josephcarlson1@gmail.com>
2014-01-23 01:51:23 -0600

    added html editor 4

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,7 @@
 CallMeJoe::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
   config.assets.precompile += [ /.ckeditor.js/, /.config.js/, /.cs.js/, /.editor.css/, /.plugin.js/, /.en.js/, /.contents.css/, /.default.js/ ]
-  
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
@@ -28,7 +28,7 @@ CallMeJoe::Application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs.
   config.assets.digest = true
